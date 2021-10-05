91a9a7c5d57d6e948d92eb2340b9b30ac6a4f9e1
Jonathan Brant <jbrant1995@gmail.com>
2021-10-05 15:36:04 -0300

    Initial commit

---
+++ src/main/java/br/com/minhaempresa/service/CalculadoraService.java
@@ -0,0 +1,50 @@
+package br.com.minhaempresa.service;
+
+import br.com.minhaempresa.domain.Operacao;
+
+public class CalculadoraService {
+
+    public double calcular(double operandoA, double operandoB, Operacao operacao){
+        double resultado = 0;
+
+        switch (operacao){
+            case SOMA: {
+                resultado = somar(operandoA, operandoB);
+                break;
+            }
+            case SUBTRACAO: {
+                resultado = subtrair(operandoA, operandoB);
+                break;
+            }
+            case MULTIPLICACAO: {
+                resultado = multiplicar(operandoA, operandoB);
+                break;
+            }
+            case DIVISAO: {
+                resultado = dividir(operandoA, operandoB);
+                break;
+            }
+            default:{}
+        }
+        return resultado;
+    }
+
+    private double somar(double operandoA, double operandoB) {
+        return operandoA + operandoB;
+    }
+
+    private double subtrair(double operandoA, double operandoB) {
+        return operandoA - operandoB;
+    }
+
+    private double multiplicar(double operandoA, double operandoB) {
+        return operandoA * operandoB;
+    }
+
+    private double dividir(double operandoA, double operandoB) throws ArithmeticException{
+        if(operandoB == 0) {
+            throw new ArithmeticException("Não foi possível realizar o calculo");
+        }
+        return operandoA / operandoB;
+    }
+}
